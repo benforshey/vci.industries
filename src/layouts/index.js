6d500181581a0160194c0c0be7c723c793bdbcfc
Ben Forshey <hello@integrisweb.com>
2017-11-16 09:00:08 -0500

    add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,6 +14,16 @@ const TemplateWrapper = ({ children, data }) => {
       >
         <html lang={`en`} />
         <meta name='description' content={data.site.siteMetadata.description} />
+
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#3cb149' />
+        <meta name='apple-mobile-web-app-title' content='VCI' />
+        <meta name='application-name' content='VCI' />
+        <meta name='theme-color' content='#3cb149' />
+
         <script type='application/ld+json'>
           {`{"@context":"http://schema.org", "@type":"WebSite", "url":"https://www.vci.industries", "name":"Victor Concrete, Inc. (VCI)", "description":"${data.site.siteMetadata.description}", "image":"${logo}"}`}
         </script>
