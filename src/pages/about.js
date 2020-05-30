3bb5a2a2e24cd8e4e6e01e5ce52a505ba93d4e75
Ben Forshey <ben@integrisweb.com>
2020-05-30 11:56:39 -0400

    Use Allow Attribute (#7)

--- src/pages/about.js
+++ src/pages/about.js
@@ -47,6 +47,7 @@ const AboutPage = () => {
               webkitallowfullscreen
               mozallowfullscreen
               allowfullscreen
+              allow="fullscreen"
             />
           </Wrapper>
         </Section>
